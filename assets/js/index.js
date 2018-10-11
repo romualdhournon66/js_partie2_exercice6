d02b3a5b8b1b914a08464ebb10e8c040abece4e4
romualdhournon66 <k2001@cegetel.net>
2018-10-11 21:27:14 +0200

    mon assets

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function changeImage(image){
+  document.getElementById(image).src='assets/img/'+image+'_2.jpg';
+}
+
+function changeImageOut(image){
+  document.getElementById(image).src='assets/img/'+image+'.jpg';
+}
